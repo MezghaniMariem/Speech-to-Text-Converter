820cee15c66e2ba791942318c0569449d82fef85
MezghaniMariem <mariem.mezghani2016@gmail.com>
2022-05-03 22:39:00 +0200

    first commit

---
+++ import speech_recognition.py
@@ -0,0 +1,19 @@
+import speech_recognition
+
+def record_voice():
+	microphone = speech_recognition.Recognizer()	
+
+	with speech_recognition.Microphone() as live_phone:
+		microphone.adjust_for_ambient_noise(live_phone)
+
+		print("You can talk now !!")
+		audio = microphone.listen(live_phone)
+		try:
+			phrase = microphone.recognize_google(audio, language='en')
+			return phrase
+		except speech_recognition.UnkownValueError:
+			return "I didn't understand what you said"
+
+if __name__ == '__main__':
+	phrase = record_voice()
+	print('the last sentence you spoke is : ',phrase)
